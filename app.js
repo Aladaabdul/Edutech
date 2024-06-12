6d04ff8149eeef9b392038c50219792fa156f76f
Emmanuel Olabisi <olaty21bisi@gmail.com>
2024-06-12 04:37:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const userRouter = require("./routes/user-routes")
 
 const app = express()
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 connectTomongo();
 app.use(express.json())
 
